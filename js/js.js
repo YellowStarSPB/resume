121445baca3a69bde4076c6b765fe9a089e9225c
YellowSar <hsgopoke@gmail.com>
2022-11-16 22:08:25 +0300

    first commit

---
+++ js/js.js
@@ -0,0 +1,28 @@
+const dino = document.getElementById("dino");
+const cactus = document.getElementById("cactus");
+
+document.addEventListener('keydown', function(event){
+    jump();
+});
+
+function jump() {
+    if(dino.classList != "jump") {
+        dino.classList.add("jump")
+    }
+    setTimeout(function(){
+        dino.classList.remove("jump")
+    }, 300)
+};
+
+let isALive = setInterval (function(){
+    let dinoTop = parseInt(window.getComputedStyle(dino).getPropertyValue("top"));
+    let cactusLeft = parseInt(window.getComputedStyle(cactus).getPropertyValue("left"));
+    let cactusRight = parseInt(window.getComputedStyle(cactus).getPropertyValue("right"));
+
+    if(cactusLeft < 40 && cactusLeft > 0 && dinoTop >= 140){
+        console.log('Вы проиграли:)')
+    }
+}, 10)
+
+
+
